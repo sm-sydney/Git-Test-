40d6589202e27de6f7d5a2288988512525b62e05
Sydney Marshall <tmq265@ut.utm.edu>
2024-01-25 15:07:15 -0600

    Added product function and output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//comment to see git diff 
 int sum (int n){
   int total = 0;
 
@@ -11,15 +11,33 @@ int sum (int n){
   return total;
 }
 
+int product (int x){
+  int total = 1;
+
+  for (int i = 1; i <=x; i++){
+    total *= i;
+  }
+
+  return total; 
+}
+
 int main (){
   int n;
-  cout << "Enter a number: ";;
+  cout << "Enter a number for sum: ";
   cin >> n;
 
   int count = sum(n);
 
   cout << count << endl; 
 
+  int x;
+  cout << "Enter a number for product: ";
+  cin >> x;
+
+  int times = product(x);
+
+  cout << times << endl;
+  
   return 0; 
 
 
