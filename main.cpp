c086298f8ec377b53577f0504d411df20c989a35
Sydney Marshall <tmq265@ut.utm.edu>
2024-01-25 11:57:51 -0600

    Completed task to ask user for n and print out the sum from 1 to n

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
-
 #include <iostream>
-
 using namespace std;
 
-int main() {
-  cout << "Hello World" << endl; 
-  
-  return 0;
+int sum (int n){
+  int total = 0;
+
+  for (int i = 1; i <=n; i++){
+    total += i;
+  }
+
+  return total;
+}
+
+int main (){
+  int n;
+  cout << "Enter a number: ";;
+  cin >> n;
+
+  int count = sum(n);
+
+  cout << count << endl; 
+
+  return 0; 
+
+
 }
